325cda54d0c0cdd1ccdfe7ef7387506b232b9c69
Caue Robattini <cauegr18@gmail.com>
2025-06-21 16:31:11 -0300

    Verifica horas com localtime

---
+++ Prova_GC/src/application/CheckHours.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.time.LocalTime;
+
+public class CheckHours {
+
+	public static void main(String[] args) {
+		LocalTime envio = LocalTime.of(23, 0);
+		LocalTime limite = LocalTime.of(22, 59);
+		
+		if(!envio.isBefore(limite)) {
+			System.out.println("Entrega fora do horário. ");
+			
+		}else {
+			System.out.println("Tarefa enviada com sucesso! ");
+			
+		}
+		
+		//Entrega fora do horário. 
+
+
+	}
+
+}
