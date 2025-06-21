127ba154a4307a8be767e65f63227f8653a3520c
Caue Robattini <cauegr18@gmail.com>
2025-06-21 15:54:28 -0300

    FluxoEOperadores

---
+++ Prova_GC/src/application/FluxoEOperadores.java
@@ -0,0 +1,18 @@
+package application;
+
+public class FluxoEOperadores {
+	public static void main(String[] args) {
+		
+		int prioridade = 4;
+		int urgência = 8;
+		
+		if(prioridade > 3 && urgência > 7) {
+			System.out.println("Tarefa Crítica");
+		}else if(prioridade > 3 || urgência > 7){
+			System.out.println("Tarefa importante");
+		}else {
+			System.out.println("Tarefa Comum");
+		}
+	}
+
+}
