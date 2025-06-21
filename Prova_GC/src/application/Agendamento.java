dd004cd640dda66f7f3ad81b537845cb9fb1d64f
Caue Robattini <cauegr18@gmail.com>
2025-06-21 15:40:26 -0300

    Questao Agendamento e avaliacao

---
+++ Prova_GC/src/application/Agendamento.java
@@ -0,0 +1,18 @@
+package application;
+
+import java.time.LocalDate;
+
+public class Agendamento {
+
+	public static void main(String[] args) {
+		LocalDate base = LocalDate.of(2025, 06, 1);
+
+		int dias = 7;
+
+		LocalDate novaData = base.plusDays(dias * 2 - 3);
+		System.out.println(novaData.getDayOfMonth());
+		
+		//Resposta: 12
+	}
+
+}
